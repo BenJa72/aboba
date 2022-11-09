dbbdca55956caf064e8243fbf5b77d86fc0a6f79
BenJa72 <113882461+BenJa72@users.noreply.github.com>
2022-11-09 14:08:38 +0500

    Update CiscoIOUKeygen3f.py

--- CiscoIOUKeygen3f.py
+++ CiscoIOUKeygen3f.py
@@ -5,14 +5,12 @@ import os
 import socket
 import hashlib
 import struct
-# get the host id and host name to calculate the hostkey
 hostid=os.popen("hostid").read().strip()
 hostname = socket.gethostname()
 ioukey=int(hostid,16)
 for x in hostname:
  ioukey = ioukey + ord(x)
 print("hostid=" + hostid +", hostname="+ hostname + ", ioukey=" + hex(ioukey)[2:])
-# create the license using md5sum
 iouPad1 = b'\x4B\x58\x21\x81\x56\x7B\x0D\xF3\x21\x43\x9B\x7E\xAC\x1D\xE6\x8A'
 iouPad2 = b'\x80' + 39*b'\0'
 md5input=iouPad1 + iouPad2 + struct.pack('!i', ioukey) + iouPad1
